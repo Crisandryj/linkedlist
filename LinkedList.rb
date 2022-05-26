202691dc495eebfeefc70b03b8430a2d5d2c8ab8
Crisandry Javier <crisandryj@gmail.com>
2022-05-26 09:08:39 -0400

    cleaned code

--- LinkedList.rb
+++ LinkedList.rb
@@ -9,7 +9,6 @@ class LinkedList
   # adds a new node containing value to the end of the list
   def append(value)
     new_node = Node.new(value)
-
   end
 
   # prepend(value) adds a new node containing value to the start of the list
@@ -25,38 +24,32 @@ class LinkedList
     until current_counter = nil
       count += 1
       current_counter = current_counter.next_node
-    end 
+    end
     count
   end
 
   # head returns the first node in the list
-  def head
-  @head
-  end
+  attr_reader :head
 
   # tail returns the last node in the list
-  def tail
-    @tail
-  end
+  attr_reader :tail
 
   # at(index) returns the node at the given index
   def at(index)
-  counter = 0
-  current_node = @head
-  until counter = index
-    current_node = @head.next_node
-    count +=1
-  end
-  current_node
+    counter = 0
+    current_node = @head
+    until counter = index
+      current_node = @head.next_node
+      count += 1
+    end
+    current_node
   end
 
   # pop removes the last element from the list
   def pop
     nil if size < 1
     current_node = @head
-    until current_node.next_node = nil
-      current_node = current_node.next_node
-    end 
+    current_node = current_node.next_node until current_node.next_node = nil
     @tail = current_node
     @tail.next_node = nil
   end
@@ -68,7 +61,7 @@ class LinkedList
     until current_node.nil?
       contains = true if current_node == value
       current_node = current_node.next_node
-    end 
+    end
     contains
   end
 
@@ -77,11 +70,12 @@ class LinkedList
     current_node = @head
     count = 0
     until current_node.data == value
-      return if nil 
+      return if nil
+
       current_node = current_node.next_node
       count += 1
       count
-    end 
+    end
   end
 
   # to_s represent your LinkedList objects as strings, so you can print them out and preview them in the console. The format should be: ( value ) -> ( value ) -> ( value ) -> nil
@@ -90,8 +84,7 @@ class LinkedList
     until current_node.nil?
       print "( #{current_node.data} ) -> "
       current_node = current_node.next_node
+    end
+    print nil
   end
-   print nil
-  end
-
 end
